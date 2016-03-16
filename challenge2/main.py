cf44e0e7eadf3dc6d80bbbf432abfe5fab4d0108
Tyler Wright <tw39440@gmail.com>
2016-03-16 17:36:00 -0400

    Delete main.py

--- challenge2/main.py
+++
@@ -1,8 +0,0 @@
-//To use this import properly, you need to execute this command:
-//python -m pip install TheBlueAlliance
-from TheBlueAlliance import *
-
-print("Zach is scrub")
-event_code = get_events_and_codes(2016, 'Mount Olive')[1]
-event = Event('github_user', 'test', '1.0', event_code)
-event.get_event_info()
