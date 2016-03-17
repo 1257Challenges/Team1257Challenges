1328a1e7f0b061e42269cbe02cfa7a7eae51507c
zbreit18 <zbreit18@gmail.com>
2016-03-17 17:23:13 -0400

    Stylistic Changes

--- challenge2/Test.php
+++ challenge2/Test.php
@@ -1,25 +1,9 @@
 <!DOCTYPE HTML> 
 <html>
 <head>
-<link href='https://fonts.googleapis.com/css?family=Slabo+27px|Roboto:400,100,300' rel='stylesheet' type='text/css'>
-<style>
-.error {color: #FF0000;}
-body { 
-background-image: url("http://p1.pichost.me/i/75/1995325.jpg");
-color: white;
-text-align: center;
-font-family: 'Roboto', sans-serif;
-font-size: 200%;
-font-weight: 100;
-}
-h2 {
-color: white;
-text-align: center;
-font-family: 'Roboto', sans-serif;
-font-size: 200%;
-font-weight: 100;
-}
-</style>
+<link href='https://fonts.googleapis.com/css?family=Cantarell|Quicksand:400,300,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="main.css" type="text/css" />
+
 </head>
 <body> 
 
@@ -30,7 +14,7 @@ $name = $number = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    if (empty($_POST["name"])) {
-     $nameErr = "Name is required";
+     $nameErr = "Competition is required";
    } else {
      $name = test_input($_POST["name"]);
      // check if name only contains letters and whitespace
@@ -67,10 +51,10 @@ function test_input($data) {
 <h2>Team 1257 Challenge App</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
    Competition: <input type="text" name="name" value="<?php echo $name;?>">
-   <span class="error">* <?php echo $nameErr;?></span>
+   <span class="error">*<?php echo $nameErr;?></span>
    <br><br>
    Number: <input type="text" name="number" value="<?php echo $number;?>">
-   <span class="error">* <?php echo $numberErr;?></span>
+   <span class="error">*<?php echo $numberErr;?></span>
    <br><br>
    <input type="submit" name="submit" value="Submit"> 
 </form>
@@ -80,5 +64,8 @@ echo "<h2>Your Output:</h2>";
 echo "$name $number";
 echo "<br>";
 
-echo exec('/etc/frc/teaminfo.sh $name $number');
+echo exec('./teaminfo.sh $name $number');
 ?>
+
+<p contenteditable="true">Bla</p>
+</body>
