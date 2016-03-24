9cabf490bfc69e4fafd45f8bd8237f7a9aa52e11
Tyler Wright <tw39440@gmail.com>
2016-03-24 16:41:51 -0400

    Copy of sh from challenge 2, needs modification

---
+++ challenge3/getData.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#frc tool
+#dependencies:
+#curl / wget
+#jq
+#this
+
+name="https://www.thebluealliance.com"
+apiVersion="v2"
+
+authString="frc1257:challengeapp:1c"
+
+outputFile=$1$2
+
+if [ -f $outputFile ]
+then
+        rm $outputFile
+fi
+
+curl -s -o $outputFile $name/api/$apiVersion/team/$1$2?X-TBA-App-Id=$authString
+
+cat $outputFile |  jq .
