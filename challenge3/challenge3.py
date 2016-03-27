8ecb695d9ce02357dd148e52ab497f351b3bf036
Tyler Wright <tw39440@gmail.com>
2016-03-27 13:48:14 -0400

    Used API to THEORETICALLY get ccwm, wins, losses
    Needs to be checked and tested

--- challenge3/challenge3.py
+++ challenge3/challenge3.py
@@ -1,6 +1,20 @@
 import math, json
+from TheBlueAlliance.Blue_Alliance_API import *
 
-def findVictoriesAmount(ccwm1, ccwm2, ccwm3, wins1, wins2, wins3, losses1, losses2, losses3):
+def winCounter(team):
+	
+
+def findVictoriesAmount(team1, team2, team3):
+	global ccwm1, ccwm2, ccwm3, wins1, wins2, wins3, losses1, losses2, losses3
+	ccwm1 = get_statistics(team1)[3]
+	ccwm2 = get_statistics(team2)[3]
+	ccwm3 = get_statistics(team3)[3]
+	wins1 = get_matches(team1, 'qm')[6]
+	wins2 = get_matches(team2, 'qm')[6]
+	wins3 = get_matches(team3, 'qm')[6]
+	losses1 = get_matches(team1, 'qm')[1] - wins1 #may actually be getting total matches in competition (documentation not specific)
+	losses2 = get_matches(team2, 'qm')[1] - wins2
+	losses3 = get_matches(team3, 'qm')[1] - wins3
 	lossesBeingZero = False
 	if losses1 != 0:
 		wl1 = wins1 / losses1
@@ -20,4 +34,3 @@ def findVictoriesAmount(ccwm1, ccwm2, ccwm3, wins1, wins2, wins3, losses1, losse
 	else:
 	# If any of the losses values are zero, returns zero to signify impossible functions
 		return 0
-
